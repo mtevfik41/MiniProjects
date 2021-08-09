aa597f1329c19820e9a277766df28bacc76f0a81
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-10 01:31:52 +0300

    href

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Grid from './components/Grid'
 function App() {
 
     const DATA = [
-        {title: 'Expanding Cards', href: process.env.PUBLIC_URL + 'Projects/ExpandingCards/index.html'},
-        {title: 'Progress Steps', href: process.env.PUBLIC_URL + 'Projects/ProgressSteps/index.html'},
-        {title: 'Rotating Navigation', href: process.env.PUBLIC_URL + 'Projects/RotatingNavigation/index.html'},
-        {title: 'Hidden Search Widget', href: process.env.PUBLIC_URL + 'Projects/HiddenSearchWidget/index.html'},
-        {title: 'Blurry Loading', href: process.env.PUBLIC_URL + 'Projects/BlurryLoading/index.html'},
-        {title: 'Scroll Animation', href: process.env.PUBLIC_URL + 'Projects/ScrollAnimation/index.html'},
-        {title: 'Form Wave Animation', href: process.env.PUBLIC_URL + 'Projects/FormWaveAnimation/index.html'},
+        { title: 'Expanding Cards', href: process.env.PUBLIC_URL + '/Projects/ExpandingCards/index.html' },
+        { title: 'Progress Steps', href: process.env.PUBLIC_URL + '/Projects/ProgressSteps/index.html' },
+        { title: 'Rotating Navigation', href: process.env.PUBLIC_URL + '/Projects/RotatingNavigation/index.html' },
+        { title: 'Hidden Search Widget', href: process.env.PUBLIC_URL + '/Projects/HiddenSearchWidget/index.html' },
+        { title: 'Blurry Loading', href: process.env.PUBLIC_URL + '/Projects/BlurryLoading/index.html' },
+        { title: 'Scroll Animation', href: process.env.PUBLIC_URL + '/Projects/ScrollAnimation/index.html' },
+        { title: 'Form Wave Animation', href: process.env.PUBLIC_URL + '/Projects/FormWaveAnimation/index.html' },
 
 
     ];

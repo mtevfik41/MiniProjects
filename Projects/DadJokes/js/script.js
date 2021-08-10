3468442f29b59775bde09248201128cc941b78a1
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-10 20:50:38 +0300

    added new projects

---
+++ Projects/DadJokes/js/script.js
@@ -0,0 +1,34 @@
+const joke = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+
+generateJoke();
+jokeBtn.addEventListener('click', generateJoke)
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json'
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json())
+//         .then(data => {
+//             joke.innerHTML = data.joke
+//         });
+// }
+
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+
+    joke.innerHTML = data.joke
+}

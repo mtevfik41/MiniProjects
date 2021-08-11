0ef4bf75bfc7fbc61547024bb587eca47e0b3942
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-11 20:40:27 +0300

    Added new projects

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         { title: 'Split Landing Page', href: process.env.PUBLIC_URL + '/Projects/SplitLandingPage/index.html' },
         { title: 'Animated Navigation', href: process.env.PUBLIC_URL + '/Projects/AnimatedNavigation/index.html' },
         { title: 'Background Slider', href: process.env.PUBLIC_URL + '/Projects/BackgroundSlider/index.html' },
-        { title: 'Button RippleEffect', href: process.env.PUBLIC_URL + '/Projects/ButtonRippleEffect/index.html' },
+        { title: 'Button Ripple Effect', href: process.env.PUBLIC_URL + '/Projects/ButtonRippleEffect/index.html' },
         { title: 'Dad Jokes', href: process.env.PUBLIC_URL + '/Projects/DadJokes/index.html' },
         { title: 'Drink Water', href: process.env.PUBLIC_URL + '/Projects/DrinkWater/index.html' },
         { title: 'Event Key Codes', href: process.env.PUBLIC_URL + '/Projects/EventKeyCodes/index.html' },
@@ -26,8 +26,16 @@ function App() {
         { title: 'Random Choice Picker', href: process.env.PUBLIC_URL + '/Projects/RandomChoicePicker/index.html' },
         { title: 'Sound Board', href: process.env.PUBLIC_URL + '/Projects/SoundBoard/index.html' },
         { title: 'Theme Clock', href: process.env.PUBLIC_URL + '/Projects/ThemeClock/index.html' },
-
-
+        { title: 'Auto Text Effect', href: process.env.PUBLIC_URL + '/Projects/AutoTextEffect/index.html' },
+        { title: 'Content Placeholder', href: process.env.PUBLIC_URL + '/Projects/ContentPlaceholder/index.html' },
+        { title: 'Double Heart Click', href: process.env.PUBLIC_URL + '/Projects/DoubleHeartClick/index.html' },
+        { title: 'Double Vertical Slider', href: process.env.PUBLIC_URL + '/Projects/DoubleVerticalSlider/index.html' },
+        { title: 'Drag N Drop', href: process.env.PUBLIC_URL + '/Projects/DragNDrop/index.html' },
+        { title: 'Drawing App', href: process.env.PUBLIC_URL + '/Projects/DrawingApp/index.html' },
+        { title: 'Github Profiles', href: process.env.PUBLIC_URL + '/Projects/GithubProfiles/index.html' },
+        { title: 'Kinetic CSS Loader', href: process.env.PUBLIC_URL + '/Projects/KineticCSSLoader/index.html' },
+        { title: 'Sticky Navbar', href: process.env.PUBLIC_URL + '/Projects/StickyNavbar/index.html' },
+        { title: 'Toast Notification', href: process.env.PUBLIC_URL + '/Projects/ToastNotification/index.html' },
     ];
     return (
         <div>
